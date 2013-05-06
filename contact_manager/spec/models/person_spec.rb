cc0612287eb68b539b29eb4ddad8f0199bb62c0c
Franklin Webber <franklin.webber@gmail.com>
2013-05-06 11:24:32 -0700

    Fixes to the people controller specs to have a last name

--- contact_manager/spec/models/person_spec.rb
+++ contact_manager/spec/models/person_spec.rb
@@ -3,28 +3,33 @@ require 'spec_helper'
 describe Person do
 
   context "when created with correct params" do
-    it "is valid" do
-      pending
-      expect(Person.new).to be_valid
+    let(:params) do
+      { first_name: "Yolanda", last_name: "Davis" }
     end
 
+    let(:person) { Person.new params }
+
+    it "is valid" do
+      expect(person).to be_valid
+    end
   end
 
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:first_name) }
 
-
-  context "when missing the last name" do
+  # context "when missing the last name" do
     let(:person) { Person.new first_name: "Yolanda" }
 
     let(:attribute) { :last_name }
 
-    it_behaves_like "an invalid person"
-  end
+  #   it_behaves_like "an invalid person"
+  # end
 
-  context "when missing a first name" do
-    let(:person) { Person.new last_name: "Davis" }
+  # context "when missing a first name" do
+  #   let(:person) { Person.new last_name: "Davis" }
 
-    let(:attribute) { :first_name }
+  #   let(:attribute) { :first_name }
 
-    it_behaves_like "an invalid person"
-  end
+  #   it_behaves_like "an invalid person"
+  # end
 end
